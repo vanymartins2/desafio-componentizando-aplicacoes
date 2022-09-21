524e31bc9980da7228dc8106159004fa9f17522e
Vanessa Martins <vanymartins2@gmail.com>
2022-09-21 19:25:55 -0300

    Challenge resolved

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,53 @@
-export function Content() {
-  // Complete aqui
+import { useEffect, useState } from 'react'
+import { api } from '../services/api'
+import { MovieCard } from './MovieCard'
+
+interface MovieProps {
+  imdbID: string
+  Title: string
+  Poster: string
+  Ratings: Array<{
+    Source: string
+    Value: string
+  }>
+  Runtime: string
+}
+
+interface ContentProps {
+  title: string
+  genreId: number
+}
+
+export function Content({ title, genreId }: ContentProps) {
+  const [movies, setMovies] = useState<MovieProps[]>([])
+
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${genreId}`).then(response => {
+      setMovies(response.data)
+    })
+  }, [genreId])
+
+  return (
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span>{title}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,44 @@
-export function SideBar() {
-  // Complete aqui
+import { useEffect, useState } from 'react'
+import { api } from '../services/api'
+import { Button } from './Button'
+
+interface GenreResponseProps {
+  id: number
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family'
+  title: string
+}
+
+interface SideBarProps {
+  genreId: number
+  handleClick: (id: number) => void
+}
+
+export function SideBar({ genreId, handleClick }: SideBarProps) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([])
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data)
+    })
+  }, [])
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClick(genre.id)}
+            selected={genreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
 }
